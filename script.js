b701ee300e7cfafebd599dd848c0a0b132e4b785
jeanlacosse <jeanlacosse@gmail.com>
2022-04-01 08:50:29 -0500

    catName added in JS

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// NUMBER VARIABLES 
 console.log('hello!');
  let motorcycles = 9;
 let cars = 0
@@ -26,3 +27,24 @@ vehicles = motorcycles + cars
 console.log('Bikes: ', motorcycles);
 console.log('Cars: ', cars);
 console.log(vehicles);
+
+console.log('took softail back out the garage');
+motorcycles++
+vehicles = motorcycles + cars
+console.log( 'Total vehicles: ', vehicles)
+
+// STRING VARIABLES BELOW
+
+let catName = 'Mischa';
+let catAge = 3;
+console.log(catName);
+
+//concatenate BELOW, space added automatically afer comma
+console.log('The cat name is:', catName)
+console.log('Tha age of the cat is:', catAge;)
+//using a +, there is no space auto added w/ a + sign, needs to be added in
+let sentence = 'Sentence about a cat named: ' + catName;
+console.log( sentence );
+
+sentence = 'The cat is ' + catAge + 'years old';
+console.log( sentence );
