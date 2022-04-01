a3ffd1bfc67cfe23cfb0d200440f88f5aa04c061
jeanlacosse <jeanlacosse@gmail.com>
2022-04-01 08:35:59 -0500

    vehicles section added to JS

--- script.js
+++ script.js
@@ -1,3 +1,28 @@
 console.log('hello!');
-let testVar = 8;
-console.log(testVar);
+ let motorcycles = 9;
+let cars = 0
+let vehicles = motorcycles + cars;
+console.log( 'Total vehicles: ', vehicles)
+console.log('Had to put the soft tail in the garage');
+
+motorcycles--;
+//subtract 1 from variable
+
+console.log('Bikes: ', motorcycles);
+console.log('Cars: ', cars);
+vehicles = motorcycles + cars
+console.log('missed the bike to I bought 2 lambos');
+
+cars += 2;
+vehicles = motorcycles + cars
+console.log('Bikes: ', motorcycles);
+console.log('Cars: ', cars);
+
+console.log('I wanted a Jetta so I got one')
+cars++;
+console.log('Sold 3 bikes');
+motorcycles -= 3;
+vehicles = motorcycles + cars
+console.log('Bikes: ', motorcycles);
+console.log('Cars: ', cars);
+console.log(vehicles);
